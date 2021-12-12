6b6e204a77f4cdbc42ab1d652a5259b9808cc5cf
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-12-13 01:11:24 +0200

    55

---
+++ UnitTest13.2.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest132
+{
+	TEST_CLASS(UnitTest132)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
